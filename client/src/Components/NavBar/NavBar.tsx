34541e7e7d6f593163c3b8e74d72e8f26d1f0b69
Евгений <132214339+EvgenyYurkov123@users.noreply.github.com>
2024-06-12 22:51:19 +0300

    4

--- client/src/Components/NavBar/NavBar.tsx
+++ client/src/Components/NavBar/NavBar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
             border="1px solid #ccc"
             p={4}>
             <Flex justifyContent={'space-between'} w={'100%'}>
-                <Flex w={'60%'} align={'center'} ml={'2%'}>123</Flex>
+                <Flex w={'60%'} align={'center'} ml={'2%'}>Добро пожаловать,</Flex>
                 <Flex w={'10%'} mr={'3%'} justifyContent={'space-between'} align={'center'}>
                     <Button bg={'#FFFFFF'} w={'42px'} h={'42px'} borderRadius={'25'} position={'relative'}>
                         <Image src="./IconMenu/Message/Group1.png" alt="Icon" w={'24px'} h={'22px'} position={'absolute'} zIndex={'1'} mr={'4px'}/>
