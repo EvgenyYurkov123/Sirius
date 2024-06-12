fea7981e5a851e5e8994fcae7ea683c7f1157357
Евгений <132214339+EvgenyYurkov123@users.noreply.github.com>
2024-06-12 23:22:11 +0300

    4

--- client/src/Components/NavBar/NavBar.tsx
+++ client/src/Components/NavBar/NavBar.tsx
@@ -2,11 +2,11 @@ import { Button, Flex,Image } from "@chakra-ui/react";
 
 export default function Navbar() {
     return (
-        <Flex bg={'skyblue'} h={'70%'} maxH={'68px'} mt={'1%'} w={'100%'} borderBottomRadius={'15px'} boxShadow="0px 4px 6px rgba(0, 0, 0, 0.1)"
-            border="1px solid #ccc"
+        <Flex  h={'70%'} maxH={'68px'} mt={'1%'} w={'100%'} borderBottomRadius={'15px'} boxShadow="4px 4px 6px rgba(0, 0, 0, 0.1)"
+           
             p={4}>
             <Flex justifyContent={'space-between'} w={'100%'}>
-                <Flex w={'60%'} align={'center'} ml={'2%'}>Добро пожаловать,</Flex>
+                <Flex w={'60%'} align={'center'} ml={'2%'} fontWeight={'500'} fontSize={'24'} fontFamily={"PT Sans Caption"}>Добро пожаловать,</Flex>
                 <Flex w={'10%'} mr={'3%'} justifyContent={'space-between'} align={'center'}>
                     <Button bg={'#FFFFFF'} w={'42px'} h={'42px'} borderRadius={'25'} position={'relative'}>
                         <Image src="./IconMenu/Message/Group1.png" alt="Icon" w={'24px'} h={'22px'} position={'absolute'} zIndex={'1'} mr={'4px'}/>
