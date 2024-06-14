6caa6d4bff7f16ffa79011bf68d6604a2dd1c50d
Евгений <132214339+EvgenyYurkov123@users.noreply.github.com>
2024-06-15 00:00:49 +0300

    4

--- client/src/redux/hooks.ts
+++
@@ -1,9 +0,0 @@
-import { useSelector, useDispatch } from 'react-redux';
-import type { TypedUseSelectorHook } from 'react-redux';
-import type { AppDispatch, RootState } from './store';
-import type { AnyAction, ThunkDispatch } from '@reduxjs/toolkit';
-
-type DispatchFunc = () => ThunkDispatch<RootState, any, AnyAction>;
-
-export const useAppDispatch: DispatchFunc = (): AppDispatch => useDispatch();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- client/src/redux/thunkActions.ts
+++
@@ -1,8 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-
-// TODO Mycode createAsincThunk
-export const fetchAllProducts = createAsyncThunk("products/all", async () => {
-  const response = await axios.get("http://localhost:3002/products");
-  return response.data;
-});
